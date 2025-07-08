d733532e5338b4190a66ff648873a9d63a2ca5e5
hana lee <lhn1208@naver.com>
2025-07-08 15:39:11 +0900

    feat: 도커세팅 수정

--- config/mysql.js
+++ config/mysql.js
@@ -3,10 +3,17 @@ const mysql = require('mysql2');
 
 // 디비 연결 정보
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'oa_db',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '1234',
+  database: process.env.DB_NAME || 'oa_db',
+
+  // 커넥션 풀이 가득차면 추가요청을 쌓고 기다림
+  waitForConnections: true,
+  // 최대 연결 수
+  connectionLimit: 10,
+  // 커넥션 풀이 가득차도 무제한 대기 허용
+  queueLimit: 0,
 });
 
 // 디비 연결
